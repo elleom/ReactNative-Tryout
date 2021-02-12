9822814577bb922b47d3b62d97184be0d2f21e04
el_le <el_leo_m@hotmail.com>
2021-02-12 12:27:06 +0200

    add addCoursehandler

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	/*
+	Syntax = conts + [ array, funtion] = useState( OBJECT => string, array, etc )
+	*/
+	const [enteredGoal, setEnteredGoal] = useState(""); //text is so far null so state('')
+	const [courseGoals, setCourseGoal] = useState([]) // [] stands for empty array
+
+
+	const goalInputHandler = (enteredtext) => {
+		setEnteredGoal(enteredtext);
+	};
+
+	const addGoalHandler = () => {
+
+		//Syntax: func( [... currentArray, itemToAdd ])
+		setCourseGoal([...courseGoals, enteredGoal]); // spread array
+
+	}
+
+	
+	return (
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder='course goal'
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
+				/>
+				<Button title='ADD' onPress={addGoalHandler} />
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	screen: {
+		padding: 50,
+	},
+	inputContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	input: {
+		width: "80%",
+		borderColor: "black",
+		borderBottomWidth: 1,
+	},
 });
